81559ddd0160780d353cc76f85bccd818a708787
Li-Ko Chien <>
2023-01-08 19:24:58 +0800

    upload js, css and side tags
    functions added(but got stuck ahh)

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ search.addEventListener("click", () => {
 });
 
 const apiKey = "AIzaSyDVq_iKY72RjOVcNQQR2LD2qsKmY6VyY6E";
-const query = "science fiction";
+const query = "full";
 
 fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`)
   .then((response) => response.json())
   .then((data) => {
     data.items.forEach((item) => {
-      console.log(item.volumeInfo.title);
+      // console.log(item.volumeInfo.title);
     });
   });
 
@@ -31,3 +31,51 @@ signInBtn.addEventListener("click", () => {
     signInBtn.innerText = "Sign in now";
   }
 });
+
+//shit got stuck on get the random items(with numberPicked value) in genre array
+
+const genre =
+  "FantasyAdventureRomanceContemporaryDystopianMysteryHorrorThrillerParanormalHistorical-fictionScience-fictionChildrenMemoirCookbookArtSelf-helpDevelopmentMotivationalHealthHistoryTravelGuide / How-toFamilies & RelationshipsHumor";
+
+function splitWords(str) {
+  let words = [];
+  let currentWord = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= "A" && str[i] <= "Z") {
+      if (currentWord) {
+        words.push(currentWord);
+      }
+      currentWord = str[i];
+    } else {
+      currentWord += str[i];
+    }
+  }
+  if (currentWord) {
+    words.push(currentWord);
+  }
+  return words;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const selected = [];
+const getNum = 0;
+const pick = document.getElementById("pick");
+const numberPicked = document.querySelector(".numberPicked");
+let pickNum = [];
+
+pick.addEventListener("click", () => {
+  if (numberPicked.value > 26) {
+    alert("We only provide 26 kinds of genres, sweet pie!");
+  } else {
+    for (let i = 0; i <= numberPicked.value; i++) {
+      i += splitWords(genre);
+      pickNum.push(i) 
+    }
+      console.log(pickNum);
+  }
+});
+
+
